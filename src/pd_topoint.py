c14df577abcb5e5645b75093bc2b5b8b17200214
geturin <keroll551@gmail.com>
2022-07-07 21:46:58 +0900

    .

--- src/pd_topoint.py
+++ src/pd_topoint.py
@@ -3,7 +3,7 @@ import rospy
 import numpy as np
 from std_msgs.msg import String
 import message_filters
-from geometry_msgs.msg import PoseWithCovarianceStamped,PoseStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped,PoseStamped,Pose
 import tf2_geometry_msgs
 import tf2_ros
 import math
@@ -16,6 +16,8 @@ from idPD import *
 rospy.init_node('tello_contrl',anonymous=True)
 
 ctrl =rospy.Publisher('/message',String,queue_size=1)
+error_vector_pub=rospy.Publisher("/error_vector",Pose,queue_size=1)
+
 
 #初始化tf2
 tf_buffer = tf2_ros.Buffer(rospy.Duration(2))
@@ -29,9 +31,7 @@ x=PD(P=50, D=200, scal=1)
 y=PD(P=50, D=200, scal=1)
 z=PD(P=20, D=200, scal=1)
 yaw=PD(P=0.8, D=15, scal=1)
-X=0
-Y=0
-Z=0
+
 errox=0
 erroy=0
 erroz=0
@@ -50,15 +50,6 @@ def callback(data):
         global map_pose,wait_point
         map_pose=data
         wait_point=1
-
-def tellopoint(data):
-        global X,Y,Z
-        tfpose=transform_pose(transformation, data)
-        point=tfpose.pose.position
-        X = point.x
-        Y = point.y
-        Z = point.z
-
 
 
 def get_transformation(source_frame, target_frame,
@@ -93,7 +84,7 @@ def getPoint():
     z = point.z
     yaw =  math.atan2(2*(qutar.w*qutar.z+qutar.x*qutar.y),1-2*(qutar.z*qutar.z+qutar.y*qutar.y))
     yaw = yaw*180/math.pi
-    return x,y,z,yaw
+    return x,y,z,yaw,tfpose.pose
 
 transformation=get_transformation("world","map")
 
@@ -102,11 +93,14 @@ while not rospy.is_shutdown():
 
         rviz = message_filters.Subscriber("/initialpose", PoseWithCovarianceStamped)
         rviz.registerCallback(callback)
-        #tello=message_filters.Subscriber("/orb_slam3_ros/camera", PoseStamped)
-        #tello.registerCallback(tellopoint)
+
 
         if wait_point ==1:
-            erro_x,erro_y,erro_z,erro_yaw=getPoint()
+            error_x,error_y,error_z,error_yaw,error_pose=getPoint()
+
+            #误差速度话题
+
+            error_vector_pub.publish(error_pose)
 
 
             sp_x =x.ctrl(error_x)
@@ -115,10 +109,10 @@ while not rospy.is_shutdown():
             sp_yaw = yaw.ctrl(error_yaw)
 
             #set speed max and min
-            sp_x = round(np.clip(a,-35,35))
-            sp_y = round(np.clip(b, -35, 35))
-            sp_z = round(np.clip(c, -35, 35))
-            sp_yaw = round(np.clip(d, -20, 20))
+            sp_x = round(np.clip(sp_x,-35,35))
+            sp_y = round(np.clip(sp_y, -35, 35))
+            sp_z = round(np.clip(sp_z, -35, 35))
+            sp_yaw = round(np.clip(sp_yaw, -20, 20))
 
 
             msg = 'rc {} {} {} {}'.format(
@@ -128,7 +122,6 @@ while not rospy.is_shutdown():
                     -1*sp_yaw
             )
             ctrl.publish(msg)
-
         else   :
             msg = 'rc {} {} {} {}'.format(
                 0,
